d6ebb6b0c41806cc2326ff3c7f75da91c7822b30
Dicky Geraldi <dicky.geraldi@ajaib.co.id>
2025-06-26 21:08:26 +0700

    Initial Commit

---
+++ MileTask/MileTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  MileTaskApp.swift
+//  MileTask
+//
+//  Created by asani on 26/06/25.
+//
+
+import SwiftUI
+
+@main
+struct MileTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
