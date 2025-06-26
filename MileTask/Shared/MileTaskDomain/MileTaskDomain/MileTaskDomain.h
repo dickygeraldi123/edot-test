dd76bbd8dcd0455d6bf15bf74248b114e173afdb
Dicky Geraldi <dicky.geraldi@ajaib.co.id>
2025-06-26 21:42:07 +0700

    initial project

---
+++ MileTask/Shared/MileTaskDomain/MileTaskDomain/MileTaskDomain.h
@@ -0,0 +1,18 @@
+//
+//  MileTaskDomain.h
+//  MileTaskDomain
+//
+//  Created by asani on 26/06/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MileTaskDomain.
+FOUNDATION_EXPORT double MileTaskDomainVersionNumber;
+
+//! Project version string for MileTaskDomain.
+FOUNDATION_EXPORT const unsigned char MileTaskDomainVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MileTaskDomain/PublicHeader.h>
+
+
